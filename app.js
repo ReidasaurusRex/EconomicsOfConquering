1b6bf554981f2f69ece371e334097c1ac720b9fb
ReidasaurusRex <reidhlewis91@gmail.com>
2015-01-31 11:09:54 -0800

    Finalized movement function. Need to consider refactoring to player prototype

--- app.js
+++ app.js
@@ -9,15 +9,23 @@ $(document).ready(function() {
 		this.dom.css("top", styTop);
 
 	}
-	
+
 	player1 = new Player(1, "625px", "500px");
-	console.log(player1.dom);
+	//console.log(player1.dom);
 
 	var tile = $(".tile");
 	tile.on("click", function(event) {
-		console.log($(this));
+		var difLeft = Math.abs(parseInt($(this).css("left"), 10) - parseInt(player1.dom.css("left"), 10));
+		var difTop = Math.abs(parseInt($(this).css("top"), 10)-parseInt(player1.dom.css("top"), 10));
+		var leftPar = (difLeft < 120); 
+		var topPar = (difTop < 120);
+		//console.log(difTop);
+		if (   (topPar && leftPar) && (     (difTop != 30) || (difLeft != 30)     )     ) {
 		player1.dom.css("left", ((parseInt(($(this).css("left")), 10) + 30)));
 		player1.dom.css("top", ((parseInt(($(this).css("top")), 10) + 30)));
+		console.log(event);
+		$(this).css("border-color", "green");
+	}
 	});
 
 });
