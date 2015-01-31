68fc1ba614567497c57481a249bb607f8b2c26f7
ReidasaurusRex <reidhlewis91@gmail.com>
2015-01-31 13:44:08 -0800

    Mobilized player 2, restricted movement to once per turn, functionalized end turn button. End turn button: allows for movement again, changes which player is moving.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	
+
+
+// *************** Player Constructor    ***************************
 function Player(num, styLeft, styTop, coLor) {
 	this.num = num;
 	this.styLeft = styLeft;
@@ -9,15 +11,17 @@ function Player(num, styLeft, styTop, coLor) {
 	this.dom.css("top", styTop);
 	this.dom.css("background-color", coLor);
 	this.coLor = coLor;
+	this.moveCounter = 0;
 
 }
+// *****************************************************************
 
 // player1 = new Player(1, "625px", "500px", "blue");
 //console.log(player1.dom);
 
 var tile = $(".tile");
 
-
+// ****************** Move Function  *******************************
 Player.prototype.move = function(thisDom) {
 	// console.log(thisDom);
 	// var self = this;
@@ -26,15 +30,22 @@ Player.prototype.move = function(thisDom) {
 		var leftPar = (difLeft < 120); 
 		var topPar = (difTop < 120);
 
-		
-			if ((topPar && leftPar) && ((difTop != 30) || (difLeft != 30))) {
-				this.dom.css("left", ((parseInt((thisDom.css("left")), 10) + 30)));
-				this.dom.css("top", ((parseInt((thisDom.css("top")), 10) + 30)));
-				console.log(event);
-				thisDom.css("border-color", this.coLor);
-				console.log(game.turnCounter);
+		if(game.turnCounter === this.num) { 
+			if (this.moveCounter === 0) {
+				if ((topPar && leftPar) && ((difTop != 30) || (difLeft != 30))) {
+					
+					this.dom.css("left", ((parseInt((thisDom.css("left")), 10) + 30)));
+					this.dom.css("top", ((parseInt((thisDom.css("top")), 10) + 30)));
+					console.log(event);
+					thisDom.css("border-color", this.coLor);
+					console.log(this.moveCounter);
+					
+					this.moveCounter++;
+				}
 			}
+		}
 };
+// *****************************************************************
 // player1.move();
 
 // tile.on("click", function() {
@@ -43,22 +54,43 @@ Player.prototype.move = function(thisDom) {
 	
 // });
 
+// ******************** Game Constructor ****************************
 function Game() {
-	this.turnCounter = 0;
+	this.turnCounter = 1;
 	this.attachListeners();
 	this.player1 = new Player(1, "465px", "340px", "red");
 	this.player2 = new Player(2, "785px", "660px", "yellow");
 }
+// ******************************************************************
 
+// *********************** Turn Function ****************************
+Game.prototype.turnChange = function() {
+	this.player1.moveCounter = 0;
+	this.player2.moveCounter = 0;
+	if (this.turnCounter === 1) {
+		this.turnCounter++;
+	}
+	else if (this.turnCounter === 2) {
+		this.turnCounter--;
+	}
+};
+
+// ******************** Listener Attach *****************************
 Game.prototype.attachListeners = function() {
 	var self = this;
-	tile.on("click", function() {
-	var tileDom = $(this);
-	self.player1.move(tileDom);
-	self.player2.move(tileDom);
-	
-});
+		
+		tile.on("click", function() {
+		var tileDom = $(this);
+		self.player1.move(tileDom);
+		self.player2.move(tileDom);
+		});
+
+		$(".turn").on("click", function() {
+			game.turnChange();
+			console.log(self.turnCounter);
+		});
 };
+// ******************************************************************
 
 Game.prototype.logic = function() {
 	var player;
