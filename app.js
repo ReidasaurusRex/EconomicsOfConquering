b708408dfb889facaee64deb1b5c199244b9fd91
ReidasaurusRex <reidhlewis91@gmail.com>
2015-01-31 13:16:12 -0800

    Added player 2. Changed starting positions. Working on adding turn and add movement to player 2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function Player(num, styLeft, styTop, coLor) {
 
 }
 
-player1 = new Player(1, "625px", "500px", "blue");
+// player1 = new Player(1, "625px", "500px", "blue");
 //console.log(player1.dom);
 
 var tile = $(".tile");
@@ -25,20 +25,61 @@ Player.prototype.move = function(thisDom) {
 		var difTop = Math.abs(parseInt(thisDom.css("top"), 10)-parseInt(this.dom.css("top"), 10));
 		var leftPar = (difLeft < 120); 
 		var topPar = (difTop < 120);
+
 		
-		if ((topPar && leftPar) && ((difTop != 30) || (difLeft != 30))) {
-			this.dom.css("left", ((parseInt((thisDom.css("left")), 10) + 30)));
-			this.dom.css("top", ((parseInt((thisDom.css("top")), 10) + 30)));
-			console.log(event);
-			thisDom.css("border-color", this.coLor);
-		}
+			if ((topPar && leftPar) && ((difTop != 30) || (difLeft != 30))) {
+				this.dom.css("left", ((parseInt((thisDom.css("left")), 10) + 30)));
+				this.dom.css("top", ((parseInt((thisDom.css("top")), 10) + 30)));
+				console.log(event);
+				thisDom.css("border-color", this.coLor);
+				console.log(game.turnCounter);
+			}
 };
 // player1.move();
 
-tile.on("click", function() {
+// tile.on("click", function() {
+// 	var tileDom = $(this);
+// 	player1.move(tileDom);
+	
+// });
+
+function Game() {
+	this.turnCounter = 0;
+	this.attachListeners();
+	this.player1 = new Player(1, "465px", "340px", "red");
+	this.player2 = new Player(2, "785px", "660px", "yellow");
+}
+
+Game.prototype.attachListeners = function() {
+	var self = this;
+	tile.on("click", function() {
 	var tileDom = $(this);
-	player1.move(tileDom);
+	self.player1.move(tileDom);
+	self.player2.move(tileDom);
 	
 });
+};
+
+Game.prototype.logic = function() {
+	var player;
+	
+	if (this.turnCounter === 1) {
+		player = this.player1;
+	}
+
+	if (this.turnCounter === 2) {
+		player = this.player2;
+	}
+};
+
+var game = new Game();
+
+
+
+
+
+
+ 
+
 
 });
