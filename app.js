dedf7a10bbd5ec164146e116449bb2e0e2416140
ReidasaurusRex <reidhlewis91@gmail.com>
2015-02-01 14:20:10 -0800

    Added a balance to player objects. Players must now purchase properties to claim, instead of simply moving into them.

--- app.js
+++ app.js
@@ -12,47 +12,48 @@ function Player(num, styLeft, styTop, coLor) {
 	this.dom.css("background-color", coLor);
 	this.coLor = coLor;
 	this.moveCounter = 0;
+	this.balance = 500;
 
 }
 // *****************************************************************
 
-// player1 = new Player(1, "625px", "500px", "blue");
-//console.log(player1.dom);
-
-var tile = $(".tile");
-
 // ****************** Move Function  *******************************
 Player.prototype.move = function(thisDom) {
-	// console.log(thisDom);
-	// var self = this;
+		var self = this;
+		// Difference between clicked tile and player left/right value
 		var difLeft = Math.abs(parseInt(thisDom.css("left"), 10) - parseInt(this.dom.css("left"), 10));
+		// Difference between clicked tile and player top/bottom value
 		var difTop = Math.abs(parseInt(thisDom.css("top"), 10)-parseInt(this.dom.css("top"), 10));
+		// Max left/right distance allowed
 		var leftPar = (difLeft < 120); 
+		// Max top/bottom difference allowed
 		var topPar = (difTop < 120);
 
+		// Moving player depending on turn
 		if(game.turnCounter === this.num) { 
+			// Ensuring player only moves once
 			if (this.moveCounter === 0) {
+				// Ensuring player move distance equals one square
 				if ((topPar && leftPar) && ((difTop != 30) || (difLeft != 30))) {
-					
-					this.dom.css("left", ((parseInt((thisDom.css("left")), 10) + 30)));
-					this.dom.css("top", ((parseInt((thisDom.css("top")), 10) + 30)));
-					console.log(event);
-					thisDom.css("border-color", this.coLor);
-					console.log(this.moveCounter);
-					
-					this.moveCounter++;
+					// Ensuring players cannot move on top of one another
+					if ((thisDom.css("left") != game.player1.dom.css("left")) && (thisDom.css("top") != game.player1.dom.css("left"))){
+						game.currentTile = thisDom;
+						this.dom.css("left", ((parseInt((thisDom.css("left")), 10) + 30)));
+						this.dom.css("top", ((parseInt((thisDom.css("top")), 10) + 30)));
+						// thisDom.addClass("player"+this.num+"Owned");
+						console.log(game.currentTile);
+						// See if state correlating to move counter
+						this.moveCounter++;
+						// Ensuring purchase prompt doesn't show if player already owns property
+						if (!thisDom.hasClass("player"+this.num+"Owned")) {
+							$(".purchasePrompt").fadeIn(500);
+						}
+					}
 				}
 			}
 		}
 };
 // *****************************************************************
-// player1.move();
-
-// tile.on("click", function() {
-// 	var tileDom = $(this);
-// 	player1.move(tileDom);
-	
-// });
 
 // ******************** Game Constructor ****************************
 function Game() {
@@ -60,6 +61,11 @@ function Game() {
 	this.attachListeners();
 	this.player1 = new Player(1, "465px", "340px", "red");
 	this.player2 = new Player(2, "785px", "660px", "yellow");
+	this.currentPlayer = this.player1;
+	setInterval(this.logic.bind(this), 1);
+	$(".purchasePrompt").hide().css("visibility", "visible");
+	this.currentTile = $(".rowC .C");
+	console.log(this.currentTile);
 }
 // ******************************************************************
 
@@ -67,43 +73,69 @@ function Game() {
 Game.prototype.turnChange = function() {
 	this.player1.moveCounter = 0;
 	this.player2.moveCounter = 0;
+	
 	if (this.turnCounter === 1) {
 		this.turnCounter++;
 	}
 	else if (this.turnCounter === 2) {
 		this.turnCounter--;
 	}
+	
+	$(".playerIndicator h3").text("Player Turn: " + this.turnCounter);
 };
+// ******************************************************************
+
+// *********************** Purchase Function ************************
+Game.prototype.purchase = function() {
+	this.currentPlayer.balance = (this.currentPlayer.balance - 100);
+	this.currentTile.addClass("player"+this.currentPlayer.num+"Owned").addClass("owned");
+	$(".purchasePrompt").fadeOut(500);
+};
+// ******************************************************************
 
 // ******************** Listener Attach *****************************
 Game.prototype.attachListeners = function() {
 	var self = this;
 		
-		tile.on("click", function() {
+		// Tile click
+		$(".tile").on("click", function() {
 		var tileDom = $(this);
-		self.player1.move(tileDom);
-		self.player2.move(tileDom);
+		self.currentPlayer.move(tileDom);
 		});
 
+		// Turn button click
 		$(".turn").on("click", function() {
 			game.turnChange();
-			console.log(self.turnCounter);
+			console.log(self.currentPlayer.balance);
+		});
+
+		// Purhcase click
+		$(".purchaseYes").on("click", function() {
+			self.purchase();
+		});
+
+		// No purchase click
+		$(".purchaseNo").on("click", function() {
+			$(".purchasePrompt").fadeOut(500);
 		});
 };
 // ******************************************************************
 
+// ******************* Logic function *******************************
 Game.prototype.logic = function() {
-	var player;
 	
 	if (this.turnCounter === 1) {
-		player = this.player1;
+		this.currentPlayer = this.player1;
 	}
 
 	if (this.turnCounter === 2) {
-		player = this.player2;
+		this.currentPlayer = this.player2;
 	}
-};
 
+	$(".playerBalances h3:first-child").text(this.player1.balance);
+	$(".playerBalances h3:last-child").text(this.player2.balance);
+};
+// ******************************************************************
 var game = new Game();
 
 
